d495c403f8456d938098a9362ae0025c5f2bc2a4
ankit <ohyouknowme623@gmail.com>
2026-06-21 12:23:17 +0000

    Add play game function to simulate 5 rounds of RPS

--- script.js
+++ script.js
@@ -20,8 +20,21 @@ function playRound(){
     // human win conditions
     if ((human === "rock" && computer === "scissors") || (human === "paper" && computer === "rock") || (human === "scissors" && computer === "paper")){
         humanScore++;
+        console.log(human + " beats " + computer);
     }
     else if ((computer === "rock" && human === "scissors") || (computer === "paper" && human === "rock") || (computer === "scissors" && human === "paper")){
         computerScore++;
+        console.log(computer + " beats " + human);
     }
+    else console.log("Draw!");
+}
+
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        playRound();
+    }
+    console.log("Final result: ");
+    if (humanScore > computerScore) console.log("You won with " + humanScore + " pts");
+    else if (computerScore > humanScore) console.log("You lost with " + humanScore + " pts");
+    else console.log("Its a draw");
 }
