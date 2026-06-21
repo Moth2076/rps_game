d4b1430671967589555ea656d0d146a2f17217e8
ankit <ohyouknowme623@gmail.com>
2026-06-21 06:42:18 +0000

    Changed human choice func to return only lowercase
    using toLowercase. and also change computer choice outputs to lowercase.
    So we can handle "ROCK", "rOck" etc.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getComputerChoice(){
     // get random number from 0 to 2
     let n = Math.floor(Math.random() * 3);
-    if (n == 0) return "Rock"
-    else if (n == 1) return "Paper"
-    return "Scissors"
+    if (n == 0) return "rock"
+    else if (n == 1) return "paper"
+    return "scissors"
 }
 
 function getHumanChoice(){
-    return prompt("Your choice? ")
+    return prompt("Your choice? ").toLowerCase();
 }
