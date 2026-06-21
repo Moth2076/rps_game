7a87903d1f3d0c715ad512293909d0cef3eb27a7
ankit <ohyouknowme623@gmail.com>
2026-06-21 12:12:23 +0000

    Add missing semicolons in computer func returns

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getComputerChoice(){
     // get random number from 0 to 2
     let n = Math.floor(Math.random() * 3);
-    if (n == 0) return "rock"
-    else if (n == 1) return "paper"
-    return "scissors"
+    if (n == 0) return "rock";
+    else if (n == 1) return "paper";
+    return "scissors";
 }
 
 function getHumanChoice(){
